912aa91cc23888e1d2d54e209e70ed3a79efc232
Pr0xximity999 <tomsomgamer999@gmail.com>
2025-03-24 14:36:04 +0100

    added dependency injections to the service builder

--- backend/droeftoeters-api/droeftoeters-api/Program.cs
+++ backend/droeftoeters-api/droeftoeters-api/Program.cs
@@ -1,6 +1,17 @@
+using droeftoeters_api.Data;
+using droeftoeters_api.Interfaces;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add config support and logging
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+builder.Logging.ClearProviders().AddConsole();
+
+//Adds the data repositories
+builder.Services.AddTransient<IDataService, SqlDataService>();
+builder.Services.AddTransient<IProcedureData, ProcedureData>();
+builder.Services.AddTransient<IProcedureItemData, ProcedureItemData>();
 
 // Add services to the container.
 builder.Services.AddAuthorization();

dcbc5bc9ccfdccd62fb7c989fe6a5d80f0952541
Pr0xximity999 <tomsomgamer999@gmail.com>
2025-03-25 16:51:47 +0100

    merging removed dependency injection, added them back

--- backend/droeftoeters-api/droeftoeters-api/Program.cs
+++ backend/droeftoeters-api/droeftoeters-api/Program.cs
@@ -1,7 +1,13 @@
+using droeftoeters_api.Data;
+using droeftoeters_api.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+//Dependicy injection
+builder.Services.AddTransient<IDataService, SqlDataService>();
+builder.Services.AddTransient<IProcedureData, ProcedureData>();
+builder.Services.AddTransient<IProcedureItemData, ProcedureItemData>();
 
 // Add services to the container.
 builder.Services.AddAuthorization();
